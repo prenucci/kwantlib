236cf2a29481e3a0d54d7d088e1d6f6b014d8ee8
Cnernc <renuccip@icloud.com>
2025-01-17 03:47:32 +0100

    refactoring monkeypatch

--- kwantlib/__init__.py
+++ kwantlib/__init__.py
@@ -1,5 +1,5 @@
 from .utilitaires import Utilitaires, monkey_patch_utilitaires
-from .operator import Operator
+from .operator import Operator, monkey_patch_operator
 from .strategyClass import Strategy
 
 __all__ = ['Utilitaires', 'Operator', 'Strategy', 'monkey_patch_utilitaires', 'monkey_patch_operator']
