2c87145bd6f75f30cd49caf6a66c4622f3a89e00
Cnernc <renuccip@icloud.com>
2025-01-20 02:33:25 +0100

    same

--- kwantlib/__init__.py
+++ kwantlib/__init__.py
@@ -1,6 +1,5 @@
 from .utilitaires import Utilitaires
 from .operators import Operator
-from .tskl_operators import tskl_Operator
 from .strategyClass import Strategy
 
-__all__ = ['Utilitaires', 'Operator', 'Strategy', 'tskl_Operator',]
+__all__ = ['Utilitaires', 'Operator', 'Strategy']
