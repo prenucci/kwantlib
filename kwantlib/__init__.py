340b37b524d5bc9ab9149dcd58639f7d2ee81a15
Cnernc <renuccip@icloud.com>
2025-01-22 11:41:58 +0100

    add Metrics

--- kwantlib/__init__.py
+++ kwantlib/__init__.py
@@ -1,5 +1,6 @@
 from .utilitaires import Utilitaires
+from .metrics import Metrics
 from .operators import Operator
 from .strategyClass import Strategy, StrategyCost
 
-__all__ = ['Utilitaires', 'Operator', 'Strategy', 'StrategyCost']
+__all__ = ['Utilitaires', 'Metrics', 'Operator', 'Strategy', 'StrategyCost']
