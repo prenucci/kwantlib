a813fb8fb373450948634addbfdb5be34702f199
Cnernc <renuccip@icloud.com>
2025-02-05 16:28:31 +0100

    add aum like backtest

--- kwantlib/core.py
+++ kwantlib/core.py
@@ -104,15 +104,12 @@ class Core:
         return - ( pnl.cumsum().cummax() - pnl.cumsum() ) 
     
     @staticmethod
-    def compute_aum(
+    def compute_cum_aum(
         pnl:pd.Series, 
-        risks:Iterable[float] = [x / 100 for x in range(1, 21, 5)],
+        risk:float = 10
     ) -> pd.DataFrame:
         
         assert isinstance(pnl, pd.Series), 'pnl must be a pd.Series'
-        pnl = pnl / pnl.std()
-        return pd.concat({
-            f'Annual vol: {100 * risk}%': (1 + (risk / 16) * pnl).cumprod()
-            for risk in risks
-        }, axis = 1)
+        pnl_scaled = (risk / 1600) * pnl / pnl.std()
+        return (1 + pnl_scaled).cumprod()
     
