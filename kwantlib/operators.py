c937fb91406434483c85df8aad6bb10c8b85750c
Cnernc <renuccip@icloud.com>
2025-04-09 11:02:08 +0200

    change is_ewm=True in cma

--- kwantlib/operators.py
+++ kwantlib/operators.py
@@ -64,7 +64,7 @@ def cross_moving_average(
         signal:pd.DataFrame | pd.Series, 
         smooth_params:Iterable[int] = (1, 2, 3, 4, 6, 8, 10,), 
         lookback_params:Iterable[int] = (2, 3, 4, 5, 6, 10, 12, 14, 17, 20, 28, 36, 44, 66,), 
-        is_proj:bool = True, is_ewm:bool = False, 
+        is_proj:bool = True, is_ewm:bool = True, 
     ) -> pd.DataFrame: 
 
     """
